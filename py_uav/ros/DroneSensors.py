573ce9a66b274c745808c924028ff7568e167054
Werikson_Alves <werikson.alves@ufv.br>
2024-11-05 18:18:43 -0300

    Remove DroneInformation class to streamline sensor management and improve code maintainability.

--- py_uav/ros/DroneSensors.py
+++ py_uav/ros/DroneSensors.py
@@ -15,10 +15,9 @@ ROS Topics (10):
     - /bebop/states/common/CommonState/WifiSignalChanged
 """
 
+import math
 import rospy
 from ..interfaces.RosCommunication import RosCommunication
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import JointState, NavSatFix
 from bebop_msgs.msg import (
     Ardrone3PilotingStateAltitudeChanged,
     Ardrone3PilotingStateAttitudeChanged,
@@ -28,18 +27,20 @@ from bebop_msgs.msg import (
     CommonCommonStateBatteryStateChanged,
     CommonCommonStateWifiSignalChanged,
 )
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import JointState, NavSatFix
+from typing import Any, Dict, List, Callable
 
 
 class DroneSensors(RosCommunication):
     """
     Manages and updates Bebop2's sensor data via ROS topics, including
-    odometry, GPS, altitude, attitude, speed, battery level, and WiFi signal
-    strength.
+    odometry, GPS, altitude, attitude, speed, battery level, and WiFi signal.
     """
 
     def __init__(self, drone_type: str, frequency: int = 30):
         """
-        Initializes Sensors class with ROS subscribers for each drone sensor.
+        Initializes the DroneSensors class with ROS subscribers for each drone sensor.
 
         :param drone_type: Type of the drone.
         :param frequency: Sensor update frequency in Hz (default: 30 Hz).
@@ -47,149 +48,188 @@ class DroneSensors(RosCommunication):
         super().__init__(drone_type, frequency)
         self.sensor_manager = SensorDataManager(update_interval=1 / frequency)
 
-        self._initialize_subscribers()
-        rospy.loginfo(f"Sensors initialized for {self.drone_type}.")
+        self._setup_subscribers()
+        rospy.loginfo(f"Sensors initialized for {drone_type}.")
 
-    def _initialize_subscribers(self) -> None:
-        """Sets up ROS subscribers for sensor data topics."""
-        rospy.Subscriber('/bebop/fix', NavSatFix, self._gps_callback)
-        rospy.Subscriber('/bebop/joint_states', JointState,
-                         self._joint_states_callback)
-        rospy.Subscriber('/bebop/odom', Odometry, self._odom_callback)
-        rospy.Subscriber(
-            '/bebop/states/ardrone3/PilotingState/AltitudeChanged',
-            Ardrone3PilotingStateAltitudeChanged, self._altitude_callback)
-        rospy.Subscriber(
-            '/bebop/states/ardrone3/PilotingState/AttitudeChanged',
-            Ardrone3PilotingStateAttitudeChanged, self._attitude_callback)
-        rospy.Subscriber(
-            '/bebop/states/ardrone3/PilotingState/PositionChanged',
-            Ardrone3PilotingStatePositionChanged, self._position_callback)
-        rospy.Subscriber(
-            '/bebop/states/ardrone3/PilotingState/SpeedChanged',
-            Ardrone3PilotingStateSpeedChanged, self._speed_callback)
-        rospy.Subscriber(
-            '/bebop/states/ardrone3/PilotingState/FlyingStateChanged',
-            Ardrone3PilotingStateFlyingStateChanged,
-            self._flying_state_callback)
-        rospy.Subscriber(
-            '/bebop/states/common/CommonState/BatteryStateChanged',
-            CommonCommonStateBatteryStateChanged, self._battery_callback)
-        rospy.Subscriber(
-            '/bebop/states/common/CommonState/WifiSignalChanged',
-            CommonCommonStateWifiSignalChanged, self._wifi_callback)
+    def _setup_subscribers(self) -> None:
+        """Configures ROS subscribers for each sensor topic."""
+        topic_map = {
+            '/bebop/fix': (NavSatFix, self._process_gps_position),
+            '/bebop/joint_states': (JointState, self._process_joint_state),
+            '/bebop/odom': (Odometry, self._process_odometry),
+            '/bebop/states/ardrone3/PilotingState/AltitudeChanged':
+                (Ardrone3PilotingStateAltitudeChanged, self._process_altitude),
+            '/bebop/states/ardrone3/PilotingState/AttitudeChanged':
+                (Ardrone3PilotingStateAttitudeChanged, self._process_attitude),
+            '/bebop/states/ardrone3/PilotingState/PositionChanged':
+                (Ardrone3PilotingStatePositionChanged, self._process_position),
+            '/bebop/states/ardrone3/PilotingState/SpeedChanged':
+                (Ardrone3PilotingStateSpeedChanged, self._process_speed),
+            '/bebop/states/ardrone3/PilotingState/FlyingStateChanged':
+                (Ardrone3PilotingStateFlyingStateChanged,
+                 self._process_flying_state),
+            '/bebop/states/common/CommonState/BatteryStateChanged':
+                (CommonCommonStateBatteryStateChanged,
+                 self._process_battery_level),
+            '/bebop/states/common/CommonState/WifiSignalChanged':
+                (CommonCommonStateWifiSignalChanged,
+                 self._process_wifi_signal),
+        }
+        for topic, (msg_type, callback) in topic_map.items():
+            rospy.Subscriber(topic, msg_type, callback)
 
-    def _initialize_publishers(self) -> None:
-        return super()._initialize_publishers()
+    # Sensor data processing methods
+    def _process_odometry(self, odom: Odometry) -> None:
+        """Processes odometry-based sensor data."""
+        self.sensor_manager.update_sensor("position", odom,
+                                          self._extract_position)
+        self.sensor_manager.update_sensor("orientation", odom,
+                                          self._extract_orientation)
+        self.sensor_manager.update_sensor("speed_linear", odom,
+                                          self._extract_linear_speed)
+        self.sensor_manager.update_sensor("speed_angular", odom,
+                                          self._extract_angular_speed)
 
-    # Sensor callback methods
+    def _process_gps_position(self, gps_data: NavSatFix) -> None:
+        """Processes GPS position data."""
+        self.sensor_manager.update_sensor("gps_position", gps_data,
+                                          self._extract_gps_position)
 
-    def _odom_callback(self, data: Odometry) -> None:
-        """Updates odometry data when due."""
-        self.sensor_manager.update_sensor_if_due("odom", data)
+    def _process_joint_state(self, joint_state: JointState) -> None:
+        """Processes joint state data."""
+        self.sensor_manager.update_sensor("joint_state", joint_state,
+                                          self._extract_joint_state)
 
-    def _gps_callback(self, data: NavSatFix) -> None:
-        """Updates GPS data when due."""
-        self.sensor_manager.update_sensor_if_due("gps", data)
-
-    def _joint_states_callback(self, data: JointState) -> None:
-        """Updates joint state data when due."""
-        self.sensor_manager.update_sensor_if_due("joint_states", data)
-
-    def _altitude_callback(self, data: Ardrone3PilotingStateAltitudeChanged
-                           ) -> None:
-        """Updates altitude data when due."""
-        self.sensor_manager.update_sensor_if_due("altitude", data.altitude)
-
-    def _attitude_callback(self, data: Ardrone3PilotingStateAttitudeChanged
-                           ) -> None:
-        """Updates attitude data (roll, pitch, yaw) when due."""
-        attitude_data = {'roll': data.roll, 'pitch': data.pitch,
-                         'yaw': data.yaw}
-        self.sensor_manager.update_sensor_if_due("attitude", attitude_data)
-
-    def _position_callback(self, data: Ardrone3PilotingStatePositionChanged
-                           ) -> None:
-        """Updates position data (latitude, longitude, altitude) when due."""
-        position_data = {'latitude': data.latitude,
-                         'longitude': data.longitude,
-                         'altitude': data.altitude}
-        self.sensor_manager.update_sensor_if_due("position", position_data)
-
-    def _speed_callback(self, data: Ardrone3PilotingStateSpeedChanged) -> None:
-        """Updates speed data (vx, vy, vz) when due."""
-        speed_data = {'vx': data.speedX, 'vy': data.speedY, 'vz': data.speedZ}
-        self.sensor_manager.update_sensor_if_due("speed", speed_data)
-
-    def _flying_state_callback(self,
-                               data: Ardrone3PilotingStateFlyingStateChanged
-                               ) -> None:
-        """Updates flying state when due."""
-        self.sensor_manager.update_sensor_if_due("flying_state", data.state)
-
-    def _battery_callback(self, data: CommonCommonStateBatteryStateChanged
+    def _process_altitude(self,
+                          altitude_data: Ardrone3PilotingStateAltitudeChanged
                           ) -> None:
-        """Updates battery level when due."""
-        self.sensor_manager.update_sensor_if_due("battery_level", data.percent)
+        """Processes altitude data."""
+        self.sensor_manager.update_sensor("altitude", altitude_data,
+                                          lambda x: x.altitude)
 
-    def _wifi_callback(self, data: CommonCommonStateWifiSignalChanged) -> None:
-        """Updates WiFi signal strength when due."""
-        self.sensor_manager.update_sensor_if_due("wifi_signal", data.rssi)
+    def _process_attitude(self,
+                          attitude_data: Ardrone3PilotingStateAttitudeChanged
+                          ) -> None:
+        """Processes attitude data."""
+        self.sensor_manager.update_sensor("attitude", attitude_data,
+                                          self._extract_attitude)
 
-    def get_raw_sensor_data(self) -> dict:
+    def _process_position(self,
+                          position_data: Ardrone3PilotingStatePositionChanged
+                          ) -> None:
+        """Processes GPS-based position data."""
+        self.sensor_manager.update_sensor("gps_position", position_data,
+                                          self._extract_gps_position)
+
+    def _process_speed(self, speed_data: Ardrone3PilotingStateSpeedChanged
+                       ) -> None:
+        """Processes speed data."""
+        self.sensor_manager.update_sensor("speed", speed_data,
+                                          self._extract_speed)
+
+    def _process_flying_state(
+            self, flying_state: Ardrone3PilotingStateFlyingStateChanged
+            ) -> None:
+        """Processes flying state data."""
+        self.sensor_manager.update_sensor("flying_state", flying_state,
+                                          lambda x: x.state)
+
+    def _process_battery_level(
+            self, battery_data: CommonCommonStateBatteryStateChanged) -> None:
+        """Processes battery level data."""
+        self.sensor_manager.update_sensor("battery_level", battery_data,
+                                          lambda x: x.percent)
+
+    def _process_wifi_signal(self,
+                             wifi_data: CommonCommonStateWifiSignalChanged
+                             ) -> None:
+        """Processes WiFi signal strength data."""
+        self.sensor_manager.update_sensor("wifi_signal", wifi_data,
+                                          lambda x: x.rssi)
+
+    # Data extraction methods
+    def _extract_position(self, odom: Odometry) -> List[float]:
+        return [odom.pose.pose.position.x, odom.pose.pose.position.y,
+                odom.pose.pose.position.z]
+
+    def _extract_orientation(self, odom: Odometry) -> List[float]:
+        x, y = odom.pose.pose.orientation.x, odom.pose.pose.orientation.y
+        z, w = odom.pose.pose.orientation.z, odom.pose.pose.orientation.w
+        return [
+            math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y)),
+            math.asin(2 * (w * y - z * x)),
+            math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z)),
+        ]
+
+    def _extract_linear_speed(self, odom: Odometry) -> List[float]:
+        return [odom.twist.twist.linear.x, odom.twist.twist.linear.y,
+                odom.twist.twist.linear.z]
+
+    def _extract_angular_speed(self, odom: Odometry) -> List[float]:
+        return [odom.twist.twist.angular.x, odom.twist.twist.angular.y,
+                odom.twist.twist.angular.z]
+
+    def _extract_gps_position(self, gps_data: NavSatFix) -> List[float]:
+        return [gps_data.latitude, gps_data.longitude, gps_data.altitude]
+
+    def _extract_joint_state(self, joint_state: JointState) -> List[float]:
+        return joint_state.position
+
+    def _extract_attitude(self,
+                          attitude_data: Ardrone3PilotingStateAttitudeChanged
+                          ) -> List[float]:
+        return [attitude_data.roll, attitude_data.pitch, attitude_data.yaw]
+
+    def _extract_speed(self, speed_data: Ardrone3PilotingStateSpeedChanged
+                       ) -> List[float]:
+        return [speed_data.speedX, speed_data.speedY, speed_data.speedZ]
+
+    def get_processed_sensor_data(self) -> Dict[str, Any]:
         """
-        Retrieves the latest sensor data.
+        Retrieves processed sensor data as a dictionary.
 
-        :return: Dictionary with current sensor readings.
+        :return: Dictionary of sensor data [position, orientation,
+                 speed_linear, speed_angular, gps_position, joint_state,
+                 altitude, attitude, speed, flying_state, battery_level,
+                 wifi_signal].
         """
         return self.sensor_manager.get_data()
 
 
 class SensorDataManager:
     """
-    Manages sensor data storage and update intervals for Bebop2's sensors.
+    Manages sensor data storage, applying data conversion and update
+    intervals for Bebop2's sensors.
     """
 
     def __init__(self, update_interval: float):
         """
-        Initializes the data manager with a specified update interval.
+        Initializes SensorDataManager with an update interval for sensor data.
 
-        :param update_interval: Minimum time interval between updates in
-        seconds.
+        :param update_interval: Time interval for updating sensor data.
         """
         self.update_interval = update_interval
         self.data = {}
         self.timestamps = {}
 
     def should_update(self, sensor_name: str) -> bool:
-        """
-        Checks if enough time has passed to update a given sensor.
-
-        :param sensor_name: Name of the sensor.
-        :return: True if time interval requirement is met, False otherwise.
-        """
+        """Checks if enough time has passed to update a given sensor."""
         current_time = rospy.get_time()
         last_update = self.timestamps.get(sensor_name)
-        if last_update is None or (current_time - last_update
-                                   ) >= self.update_interval:
+        if last_update is None or (current_time - last_update) >= self.update_interval:
             self.timestamps[sensor_name] = current_time
             return True
         return False
 
-    def update_sensor_if_due(self, sensor_name: str, data) -> None:
+    def update_sensor(self, sensor_name: str, data: Any, converter: Callable[
+            [Any], Any]) -> None:
         """
-        Updates sensor data if the update interval requirement is met.
-
-        :param sensor_name: Name of the sensor.
-        :param data: Sensor data to be stored.
+        Updates sensor data if the update interval requirement is met and
+        applies data conversion.
         """
         if self.should_update(sensor_name):
-            self.data[sensor_name] = data
+            self.data[sensor_name] = converter(data)
 
-    def get_data(self) -> dict:
-        """
-        Retrieves all stored sensor data.
-
-        :return: Dictionary with the latest sensor data.
-        """
+    def get_data(self) -> Dict[str, Any]:
+        """Retrieves all stored sensor data with converted values."""
         return self.data
